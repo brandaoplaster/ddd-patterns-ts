cf6fb83c939bbe529f0311e11a7e4a0b43db9b57
brandaoplaster <brandaoplaster@gmail.com>
2023-12-15 23:17:25 -0300

    test: Adding tests to order

--- src/tests/entity/order.spec.ts
+++ src/tests/entity/order.spec.ts
@@ -21,16 +21,23 @@ describe("Order unit test", () => {
   });
 
   it("should calculate total", () => {
-    const item = new OrderItem("1", "item 1", 100);
-    const item2 = new OrderItem("2", "item 2", 20);
+    const item = new OrderItem("1", "item 1", 100, "p1", 2);
+    const item2 = new OrderItem("2", "item 2", 200, "p2", 2);
     const order = new Order("1", "1", [item]);
 
     let total = order.total();
 
-    expect(total).toBe(100);
+    expect(total).toBe(200);
     const order2 = new Order("2", "2", [item, item2]);
     total = order2.total();
 
-    expect(total).toBe(120);
+    expect(total).toBe(600);
+  });
+
+  it("should throw error if the item quantity is greater than 0", () => {
+    expect(() => {
+      const item = new OrderItem("1", "item 1", 100, "p1", 0);
+      const order = new Order("1", "1", [item]);
+    }).toThrow("Quantity must be greater than 0");
   });
 });
