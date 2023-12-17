8c663401611ae5b4787856c1ea9ca024b6e2e3e3
brandaoplaster <brandaoplaster@gmail.com>
2023-12-16 23:07:47 -0300

    test: Adding tests to product service

---
+++ src/tests/services/product.service.spec.ts
@@ -0,0 +1,16 @@
+import Product from "../../entity/product";
+import ProductService from "../../services/product.service";
+
+describe("Product service unit tests", () => {
+  it("should change the prices of all products", () => {
+    const product1 = new Product("123", "p1", 10);
+    const product2 = new Product("124", "p2", 20);
+
+    const products = [product1, product2]
+
+    ProductService.incrementPrice(products, 100);
+
+    expect(product1.price).toBe(20);
+    expect(product2.price).toBe(40);
+  });
+});
