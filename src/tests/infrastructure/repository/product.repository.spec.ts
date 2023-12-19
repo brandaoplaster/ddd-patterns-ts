0687f358da45d661e04e8f0ae55cc22f6847367c
brandaoplaster <brandaoplaster@gmail.com>
2023-12-18 23:06:42 -0300

    test: Adding test to product repository create

--- src/tests/infrastructure/repository/product.repository.spec.ts
+++ src/tests/infrastructure/repository/product.repository.spec.ts
@@ -1,21 +1,38 @@
 import { Sequelize } from "sequelize-typescript";
 import ProductModel from "../../../infrastructure/db/sequilize/model/product.model";
+import Product from "../../../domain/entity/product";
+import ProductRepository from "../../../infrastructure/repository/product.repository";
 
 describe("Product repository test", () => {
-  let sequilize: Sequelize;
+  let sequileze: Sequelize;
 
   beforeEach(async () => {
-    sequilize = new Sequelize({
-      dialect: 'sqlite',
-      storage: ':memory',
+    sequileze = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
       logging: false,
-      sync: { force: true }
+      sync: { force: true },
     });
-    sequilize.addModels([ProductModel]);
-    await sequilize.sync;
+    sequileze.addModels([ProductModel]);
+    await sequileze.sync();
   });
 
   afterEach(async () => {
-    await sequilize.close;
+    await sequileze.close();
+  });
+
+  it("should create a product", async () => {
+    const productRepository = new ProductRepository();
+    const product = new Product("123", "product 1", 100);
+
+    await productRepository.create(product);
+
+    const productModel = await ProductModel.findOne({ where: { id: "123" } });
+
+    expect(productModel.toJSON()).toStrictEqual({
+      id: "123",
+      name: "product 1",
+      price: 100
+    });
   });
 });
