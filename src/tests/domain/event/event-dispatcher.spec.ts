037741a4750efecd884d42ff6c60f6873b7f7756
brandaoplaster <brandaoplaster@gmail.com>
2024-04-27 22:42:47 -0300

    test: Adding test to register an event handler

---
+++ src/tests/domain/event/event-dispatcher.spec.ts
@@ -0,0 +1,21 @@
+import EventDispatcher from "../../../domain/event/@shared/event-dispatcher";
+import SendEmailWhenProductIsCreatedHandler from "../../../domain/event/@shared/product/handler/send-email.handler";
+
+describe("Domain events tests", () => {
+  it("should register an event handler", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+    eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+    expect(
+      eventDispatcher.getEventHandlers["ProductCreatedEvent"]
+    ).toBeDefined();
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(
+      1
+    );
+    expect(
+      eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]
+    ).toMatchObject(eventHandler);
+  });
+});
