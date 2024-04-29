234eb13a49f6b63b00e3d080becc6fa4054eb6da
brandaoplaster <brandaoplaster@gmail.com>
2024-04-28 22:21:58 -0300

    test: Adding test to notify

--- src/tests/domain/event/event-dispatcher.spec.ts
+++ src/tests/domain/event/event-dispatcher.spec.ts
@@ -1,5 +1,6 @@
 import EventDispatcher from "../../../domain/event/@shared/event-dispatcher";
 import SendEmailWhenProductIsCreatedHandler from "../../../domain/event/@shared/product/handler/send-email.handler";
+import ProductCreatedEvent from "../../../domain/event/@shared/product/product-created.event";
 
 describe("Domain events tests", () => {
   it("should register an event handler", () => {
@@ -54,5 +55,27 @@ describe("Domain events tests", () => {
     expect(
       eventDispatcher.getEventHandlers["ProductCreatedEvent"]
     ).toBeUndefined();
+  });
+
+  it("should notify all event handlers", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+    const spyEventHandler = jest.spyOn(eventHandler, "handle");
+
+    eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+    expect(
+      eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]
+    ).toMatchObject(eventHandler);
+
+    const productCreatedEvent = new ProductCreatedEvent({
+      name: "Product 1",
+      description: "Product 1 description",
+      price: 10.0,
+    });
+
+    eventDispatcher.notify(productCreatedEvent);
+
+    expect(spyEventHandler).toHaveBeenCalled();
   });
 });
